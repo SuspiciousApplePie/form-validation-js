9c2c56790f41ddc745661d6b5b20382e1a6c4b30
SuspiciousApplePie <andreiusita6@gmail.com>
2026-03-29 18:22:13 +0800

    Add city selection validation

--- js/script.js
+++ js/script.js
@@ -1,41 +1,38 @@
 const cities = [
     {
         ID: 'il',
-        NAME: 'Illumia',
         POSTAL_CODE_FORMAT: /^IL-\d{3,9}$/,
     },
     {
         ID: 'lu',
-        NAME: 'Lunatown',
         POSTAL_CODE_FORMAT: /^LU-\d{3,8}(-[A-Z])?/,
     },
     {
         ID: 'st',
-        NAME: 'Starspire',
         POSTAL_CODE_FORMAT: /^ST-0{5}1$/,
     },
     {
         ID: 'fl',
-        NAME: 'Florenium',
         POSTAL_CODE_FORMAT: /^(\d{5})-FL(\d{3,5})-[ABC]$/,
     },
     {
         ID: 'um',
-        NAME: 'Ume',
         POSTAL_CODE_FORMAT: /^UM-\d{5}$/,
     }
 ];
 
-const inputWrapper = {
-    CLASS: '.form-control',
+const parentElement = {
+    INPUT_WRAPPER: '.form-control',
 }
 
 const errorMessage = {
     CLASS: '.error-message',
 }
 
-const inputEmailElement = {
-    ID: 'email-address',
+const inputElement = {
+    EMAIL: 'email-address',
+    POSTAL: '#postal',
+    CITY: 'cities',
 }
 
 function init() {
@@ -45,21 +42,43 @@ function init() {
 function setUpEventListeners() {
     const main = document.querySelector(".container");
     main.addEventListener("input", (e) => {
-        if(e.target.id === inputEmailElement.ID) {
-            checkNameInput(e.target);
+        if (e.target.id === inputElement.EMAIL) {
+            checkEmailInput(e.target);
         }
     });
     main.addEventListener("change", (e) => {
-        console.log(e);
+        if (e.target.id === inputElement.CITY) {
+            changeRegexPattern(e.target);
+            checkCitySelected(e.target);
+        }
     })
 }
 
-function checkNameInput(input) {
-    const errorMsgElement = input.closest(inputWrapper.CLASS).querySelector(errorMessage.CLASS);
+function checkEmailInput(input) {
+    const errorMsgElement = input.closest(parentElement.INPUT_WRAPPER).querySelector(errorMessage.CLASS);
     if (input.validity.valueMissing) {
         errorMsgElement.textContent = "Please enter your email.";
     } else if (input.validity.patternMismatch) {
         errorMsgElement.textContent = 'Please enter a valid Luminary account. eg: (example@luminae.lum).'
+    } else {
+        errorMsgElement.textContent = '';
+    }
+}
+
+function changeRegexPattern(input) {
+    const postalInputElement = input.closest("form").querySelector(inputElement.POSTAL);
+    postalInputElement.removeAttribute('pattern');
+    cities.forEach(city => {
+        if (city.ID === input.value) {
+            postalInputElement.setAttribute('pattern', city.POSTAL_CODE_FORMAT.source);
+        }
+    })
+}
+
+function checkCitySelected(select) {
+    const errorMsgElement = select.closest(parentElement.INPUT_WRAPPER).querySelector(errorMessage.CLASS);
+    if (select.validity.valueMissing) {
+        errorMsgElement.textContent = "Please select a city";
     } else {
         errorMsgElement.textContent = '';
     }
