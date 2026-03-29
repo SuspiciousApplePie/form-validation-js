4f17c269a82a676386e8eb6fb43082e46d90dc77
SuspiciousApplePie <andreiusita6@gmail.com>
2026-03-29 19:08:33 +0800

    Add postal code validation

--- js/script.js
+++ js/script.js
@@ -2,22 +2,27 @@ const cities = [
     {
         ID: 'il',
         POSTAL_CODE_FORMAT: /^IL-\d{3,9}$/,
+        SAMPLE: 'IL-123456789'
     },
     {
         ID: 'lu',
-        POSTAL_CODE_FORMAT: /^LU-\d{3,8}(-[A-Z])?/,
+        POSTAL_CODE_FORMAT: /^LU-\d{3,8}(-[A-Z])$/,
+        SAMPLE: 'LU-12345-A',
     },
     {
         ID: 'st',
         POSTAL_CODE_FORMAT: /^ST-0{5}1$/,
+        SAMPLE: 'ST-000001',
     },
     {
         ID: 'fl',
         POSTAL_CODE_FORMAT: /^(\d{5})-FL(\d{3,5})-[ABC]$/,
+        SAMPLE: '12345-FL-1234-C',
     },
     {
         ID: 'um',
         POSTAL_CODE_FORMAT: /^UM-\d{5}$/,
+        SAMPLE: 'UM-12345',
     }
 ];
 
@@ -44,6 +49,8 @@ function setUpEventListeners() {
     main.addEventListener("input", (e) => {
         if (e.target.id === inputElement.EMAIL) {
             checkEmailInput(e.target);
+        } else if (e.target.id === inputElement.POSTAL.replace('#', '')) {
+            checkPostalInput(e.target);
         }
     });
     main.addEventListener("change", (e) => {
@@ -71,6 +78,7 @@ function changeRegexPattern(input) {
     cities.forEach(city => {
         if (city.ID === input.value) {
             postalInputElement.setAttribute('pattern', city.POSTAL_CODE_FORMAT.source);
+            postalInputElement.placeholder = city.SAMPLE;
         }
     })
 }
@@ -79,6 +87,17 @@ function checkCitySelected(select) {
     const errorMsgElement = select.closest(parentElement.INPUT_WRAPPER).querySelector(errorMessage.CLASS);
     if (select.validity.valueMissing) {
         errorMsgElement.textContent = "Please select a city";
+    } else {
+        errorMsgElement.textContent = '';
+    }
+}
+
+function checkPostalInput(postal) {
+    const errorMsgElement = postal.closest(parentElement.INPUT_WRAPPER).querySelector(errorMessage.CLASS);
+    if (postal.validity.valueMissing) {
+        errorMsgElement.textContent = 'Please enter your postal code.';
+    } else if (postal.validity.patternMismatch) {
+        errorMsgElement.textContent = `Wrong format, the proper format is: ${postal.placeholder}`;
     } else {
         errorMsgElement.textContent = '';
     }
