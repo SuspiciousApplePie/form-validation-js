5c2797a3797c92ba323eb5bbea6533f1e5c45865
SuspiciousApplePie <andreiusita6@gmail.com>
2026-03-29 16:29:30 +0800

    Add city postal format

---
+++ js/script.js
@@ -0,0 +1,28 @@
+const cities = [
+    {
+        ID: 'il',
+        NAME: 'Illumia',
+        POSTAL_CODE_FORMAT: /^IL-\d{3,9}$/,
+    },
+    {
+        ID: 'lu',
+        NAME: 'Lunatown',
+        POSTAL_CODE_FORMAT: /^LU-\d{3,8}(-[A-Z])?/,
+    },
+    {
+        ID: 'st',
+        NAME: 'Starspire',
+        POSTAL_CODE_FORMAT: /^ST-0{5}1$/,
+    },
+    {
+        ID: 'fl',
+        NAME: 'Florenium',
+        POSTAL_CODE_FORMAT: /^(\d{5})-FL(\d{3,5})-[ABC]$/,
+    },
+    {
+        ID: 'um',
+        NAME: 'Ume',
+        POSTAL_CODE_FORMAT: /^UM-\d{5}$/,
+    }
+];
+
